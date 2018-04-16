c791b08af6e23d3087a92c8414f650660a9dd322
Unknown <Landon.wright91@gmail.com>
2018-04-16 16:54:21 -0600

    added power calcs to water reclamation stuff

--- cal_water_recycle_weight.m
+++ cal_water_recycle_weight.m
@@ -1,16 +1,18 @@
-function [ equip_weight ] = cal_water_recycle_weight( num_people, duration )
+function [ equip_weight, equip_power ] = cal_water_recycle_weight( num_people, duration )
 % cal_water_recycle_weight - calculates the weight of the equipment that is required
 % for water recycling on the mission
 % num_people: number of people that are going on the mission
 % duration: the duration of the mission in weeks
 % equip_weight: the weight in kg of the recycling equipment for the mission
-%info from 
+%info from  (pg 112 ish)
 %https://spaceflightsystems.grc.nasa.gov/repository/NRA/tm206956v1%20living%20together%20%20in%20space.pdf
 people_per_unit = 6;
 water_processor_weight = 476; %kg per six people
 urine_processor_weight = 128; %kg per six people
 annual_wp_weight = 478; %kg per six people
 annual_up_weight = 175; %kg per six people
+avg_water_power = 300; %watts
+avg_urine_power = 91; %watta
 
 num_units_required = ceil(num_people / people_per_unit);
 hardwear_weight = num_units_required * (water_processor_weight + urine_processor_weight);
@@ -22,4 +24,5 @@ consumable_interval_weight = (consumable_replacement_interval / weeks_per_year) 
 num_consumable_intervals = ceil(duration / consumable_replacement_interval);
 consumable_weight = num_consumable_intervals * consumable_interval_weight; %kg
 equip_weight = hardwear_weight + consumable_weight; % kg
+equip_power = num_units_required * (avg_urine_power + avg_water_power);
 end
